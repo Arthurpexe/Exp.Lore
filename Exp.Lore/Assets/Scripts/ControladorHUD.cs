4988ad17b29887f653732aa7b8ae84040967dd05
Arthurpexe <Arthur.moura@sga.pucminas.br>
2019-10-12 20:25:25 -0300

    fiz a parte grafica do inventario
    falta ainda a programação por cima dos graficos

---
+++ Exp.Lore/Assets/Scripts/ControladorHUD.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorHUD : MonoBehaviour
+{
+    public void abrirInventario(GameObject meuCanvas)
+    {
+        if (meuCanvas.activeSelf)
+        {
+            meuCanvas.SetActive(false);
+        }
+        else
+        {
+            meuCanvas.SetActive(true);
+        }
+    }
+}
