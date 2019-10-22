895656a2e673e9c8e125af30f278b9028d790277
Arthurpexe <48460233+Arthurpexe@users.noreply.github.com>
2019-10-22 17:26:32 -0300

    adicionei o painel de gameover
    ele ta quase completo e funcionando, falta so a parte de dar trigger no painel,
    fiz tbm uma pequena alteração no botão do menu.

---
+++ Exp.Lore/Assets/Scripts/TrocarSprite.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrocarSprite : MonoBehaviour
+{
+
+    public GameObject painelMenu;
+    public Image imagemIcone;
+    public Sprite playSprite, pauseSprite;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (painelMenu.activeSelf)
+            imagemIcone.sprite = pauseSprite;
+        else
+            imagemIcone.sprite = playSprite;
+    }
+}
