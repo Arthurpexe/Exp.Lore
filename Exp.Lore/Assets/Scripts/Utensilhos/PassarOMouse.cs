f62db0dbe8d58c08fcbc18f8379e2ca887a04952
Arthurpexe <48460233+Arthurpexe@users.noreply.github.com>
2019-11-13 18:45:44 -0300

    fiz o script de passar o mouse em cima do item
    mas um antigo bug veio me assolar novamente, mas dessa vez eu irei resolve-lo, nem que tenha que virar uma noite

---
+++ Exp.Lore/Assets/Scripts/Utensilhos/PassarOMouse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PassarOMouse : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
+{
+    public GameObject painelDescricaoItem;
+    bool mouseDentro = false;
+
+    void Update()
+    {
+        if (mouseDentro)
+        {
+            painelDescricaoItem.SetActive(true);
+        }
+        else
+        {
+            painelDescricaoItem.SetActive(false);
+        }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseDentro = true;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseDentro = false;
+    }
+}
